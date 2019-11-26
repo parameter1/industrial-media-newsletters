1f7d4b0d4cc500529a0dae32990535fae728d222
Brian Miller <bmiller@southcomm.com>
2019-11-26 09:01:56 -0600

    setup id today

---
+++ tenants/all/config/email-x.js
@@ -0,0 +1,5 @@
+const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config');
+
+const config = new EmailXConfiguration('https://indm.serve.email-x.io');
+
+module.exports = config;
