055ea428efe6e14235b57851c0a1b626e3c97b2e
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-03-10 09:44:34 -0500

    Cleanup footer/Opt-Out
    Lots of copy+pasting, so I moved it all into /components and set a couple variables in core.js for customizing across groups

--- tenants/ien/config/core.js
+++ tenants/ien/config/core.js
@@ -1,1 +1,6 @@
-module.exports = {};
+module.exports = {
+  optOut: {
+    unsubscribeLink: "%%=RedirectTo(CloudPagesURL(204, 'sk', _subscriberkey, 'p', listid, 'ln', _listname, 'j', jobid, 'e', _emailid, 'en', emailname_, 'de', _DataSourceName))=%%",
+    safeSenders: 'reply.ien.com',
+  },
+};
