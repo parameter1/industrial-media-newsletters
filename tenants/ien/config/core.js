10a1e7e5f61f323de1785d1f6c137b2c05d5ccb3
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-03-12 09:15:21 -0500

    Social Media added to core.js

--- tenants/ien/config/core.js
+++ tenants/ien/config/core.js
@@ -3,4 +3,21 @@ module.exports = {
     unsubscribeLink: "%%=RedirectTo(CloudPagesURL(204, 'sk', _subscriberkey, 'p', listid, 'ln', _listname, 'j', jobid, 'e', _emailid, 'en', emailname_, 'de', _DataSourceName))=%%",
     safeSenders: 'reply.ien.com',
   },
+  socialMediaLinks: [
+    {
+      provider: 'facebook',
+      href: 'https://www.facebook.com/Industrial-Equipment-News-153179218105048',
+      title: 'Follow us on Facebook',
+    },
+    {
+      provider: 'twitter',
+      href: 'https://twitter.com/IEN_Now',
+      title: 'Follow us on Twitter',
+    },
+    {
+      provider: 'youtube',
+      href: 'https://www.youtube.com/channel/UCwKursTtvT5lmEJBhdYeRbQ',
+      title: 'Follow us on YouTube',
+    },
+  ],
 };
