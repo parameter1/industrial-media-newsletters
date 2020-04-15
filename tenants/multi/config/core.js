3ebfb79b08dd35ab1911621e72e6880f856434b4
Jacob Bare <jacob@limit0.io>
2020-04-15 14:18:08 -0500

    Add social media links for ddt

--- tenants/multi/config/core.js
+++ tenants/multi/config/core.js
@@ -1,1 +1,21 @@
-module.exports = {};
+module.exports = {
+  ddt: {
+    socialMediaLinks: [
+      {
+        provider: 'facebook',
+        href: 'https://www.facebook.com/designdevelopmenttoday/',
+        title: 'Follow us on Facebook',
+      },
+      {
+        provider: 'twitter',
+        href: 'https://twitter.com/DesignDevelop12',
+        title: 'Follow us on Twitter',
+      },
+      {
+        provider: 'linkedin',
+        href: 'https://www.linkedin.com/groups/13611964',
+        title: 'Follow us on LinkedIn',
+      },
+    ],
+  },
+};
