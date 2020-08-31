0c62e12b6e241bc2637a247433c38cca1f77e5ca
Josh Worden <josh@parameter1.com>
2020-08-31 18:47:06 -0500

    Update default email-x domain

--- tenants/multi/config/email-x.js
+++ tenants/multi/config/email-x.js
@@ -1,6 +1,6 @@
 const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config');
 
-const config = new EmailXConfiguration('https://indm.serve.email-x.io');
+const config = new EmailXConfiguration('https://indm.serve.email-x.parameter1.com');
 
 config
   .setAdUnits('fm-today', [
