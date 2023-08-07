611d617a59b411709323397f623cbcae9975d6ae
Brian Miller <brian@parameter1.com>
2023-08-07 15:38:21 -0500

    point at parameter1 packages

--- tenants/multi/config/email-x.js
+++ tenants/multi/config/email-x.js
@@ -1,6 +1,6 @@
-const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config');
+const EmailXConfiguration = require('@parameter1/base-cms-marko-newsletters-email-x/config');
 
-const config = new EmailXConfiguration('https://indm.serve.email-x.parameter1.com');
+const config = new EmailXConfiguration(process.env.EMAILX_SERVE_URI || 'https://roguemonkeymedia.serve.email-x.parameter1.com');
 
 config
   .setAdUnits('fm-today', [
