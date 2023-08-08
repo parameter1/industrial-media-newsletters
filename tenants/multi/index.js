64cbe0e3ec20cc9858cf505f1ce1b178effeaeb9
Brian Miller <brian@parameter1.com>
2023-08-08 14:59:14 -0500

    lint:fix

--- tenants/multi/index.js
+++ tenants/multi/index.js
@@ -1,5 +1,5 @@
 const newrelic = require('newrelic');
-const { startServer } = require('@base-cms/marko-newsletters');
+const { startServer } = require('@parameter1/base-cms-marko-newsletters');
 const coreConfig = require('./config/core');
 const customConfig = require('./config/custom');
 
@@ -10,6 +10,6 @@ module.exports = startServer({
   coreConfig,
   customConfig,
   publicPath: 'public',
-  onStart: app => app.set('trust proxy', 'loopback, linklocal, uniquelocal'),
-  onAsyncBlockError: e => newrelic.noticeError(e),
-}).then(() => log('Newsletters started!')).catch(e => setImmediate(() => { throw e; }));
+  onStart: (app) => app.set('trust proxy', 'loopback, linklocal, uniquelocal'),
+  onAsyncBlockError: (e) => newrelic.noticeError(e),
+}).then(() => log('Newsletters started!')).catch((e) => setImmediate(() => { throw e; }));
